ae5fa7379bc8371e62e5c9c777b1ec82cb5254da
Leo Campos <leopcampos85@gmail.com>
2022-10-27 15:52:02 -0300

    first commit

---
+++ bytebank_ADM/Funcionarios/Funcionario.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bytebank_ADM.Funcionarios
+{
+    public class Funcionario
+    {
+        public string Nome { get; set; }
+        public string Cpf { get; set; }
+        public double Salario { get; set; }
+
+        public virtual double GetBonificacao()
+        {
+            return Salario * 0.10;
+        }
+
+        public virtual double SalarioAtualizado()
+        {
+            return Salario + GetBonificacao();
+        }
+
+        public override string ToString()
+        {
+            return Nome + ", " +  Cpf + ", " + SalarioAtualizado().ToString("C2", CultureInfo.CurrentCulture);
+        }
+    }
+}
---
+++ bytebank_ADM/Program.cs
@@ -0,0 +1,19 @@
+﻿using bytebank_ADM.Funcionarios;
+using System.Globalization;
+
+Funcionario Pedro = new Funcionario();
+Pedro.Nome = "Pedro Queiroz";
+Pedro.Cpf = "123456789";
+Pedro.Salario = 2000;
+
+Console.WriteLine("Bonificação de Pedro: " + Pedro.GetBonificacao().ToString("C2", CultureInfo.CurrentCulture));
+Console.WriteLine("Salário atualizado de Pedro: " + Pedro.SalarioAtualizado().ToString("C2", CultureInfo.CurrentCulture));
+
+Diretor diretor = new Diretor();
+diretor.Nome = "Cícero César";
+diretor.Cpf = "11445355796";
+diretor.Salario = 5000;
+
+Console.WriteLine();
+Console.WriteLine("Bonificação de Cícero: " + diretor.GetBonificacao().ToString("C2", CultureInfo.CurrentCulture));
+Console.WriteLine("Salário atualizado de Cícero: " + diretor.SalarioAtualizado().ToString("C2", CultureInfo.CurrentCulture));
